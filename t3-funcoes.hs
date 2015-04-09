54fae7505f676993bdefb72ce7b2a4ad1e9e797b
cassiandrei <cschneider@inf.ufsm.br>
2015-04-09 18:35:18 -0300

    3 pronto

--- t3-funcoes.hs
+++ t3-funcoes.hs
@@ -5,3 +5,14 @@ firstName x = if (head x) /= ' ' then head x : firstName (tail x) else []
 firstName' :: String -> String
 firstName' x = takeWhile (/= ' ') x
 
+--lastName :: String -> String
+--lastName x = if (last x) /= ' ' then last x : lastName (init x) else []
+--lastName :: String -> String
+--lastName x = reverse x
+ -- |(last x) /= ' ' = last x : lastName (init x)
+ -- |otherwise = []
+lastName :: String -> String
+lastName x 
+	| filter (==' ')  x == [] = x
+	| otherwise = lastName (tail x)
+	
