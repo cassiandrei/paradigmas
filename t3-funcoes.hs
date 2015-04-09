7dabf0ae7499156d9bfdc757e6541a963c6c6c80
cassiandrei <cschneider@inf.ufsm.br>
2015-04-09 10:14:18 -0300

    funcao 1 pronta

--- t3-funcoes.hs
+++ t3-funcoes.hs
@@ -1,5 +1,3 @@
 firstName :: String -> String
---firstName " " = []
 firstName [] = []
---firstName x = head x : firstName (tail x)
 firstName x = if (head x) /= ' ' then head x : firstName (tail x) else []
