5afe043de57c0bbf359b4aeb2ea8ecf3c41c7c20
cassiandrei <cschneider@inf.ufsm.br>
2015-04-09 10:21:51 -0300

    funcao 2 pronta

--- t3-funcoes.hs
+++ t3-funcoes.hs
@@ -1,3 +1,7 @@
 firstName :: String -> String
 firstName [] = []
 firstName x = if (head x) /= ' ' then head x : firstName (tail x) else []
+
+firstName' :: String -> String
+firstName' x = takeWhile (/= ' ') x
+
