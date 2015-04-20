943a969db4bb5ec87e48749e04c405864a633a7d
cassiandrei <cschneider@inf.ufsm.br>
2015-04-20 18:05:50 -0300

    dias

---
+++ t4-assassinato.pl
@@ -0,0 +1,50 @@
+% dias
+segunda(pedro, santa).
+segunda(caren, porto).
+segunda(henrique, ap).
+segunda(bia, ap).
+segunda(adriano, ap).
+segunda(alice, ap).
+segunda(bernado, santa).
+segunda(maria, ap).
+
+terca(caren, porto).
+terca(pedro, santa).
+terca(henrique, porto).
+terca(bia, porto).
+terca(adriano, ap).
+terca(alice, porto).
+terca(bernado, santa).
+terca(maria, santa).
+
+quarta(pedro, porto).
+quarta(caren, porto).
+quarta(henrique, ap).
+quarta(bia, porto).
+quarta(adriano, santa).
+quarta(alice, porto).
+quarta(bernado, porto).
+quarta(maria, santa).
+
+quinta(pedro, santa).
+quinta(caren, santa).
+quinta(henrique, ap).
+quinta(bia, santa).
+quinta(adriano, ap).
+quinta(alice, ap).
+quinta(bernado, santa).
+quinta(maria, santa).
+
+sexta(caren, ap).
+sexta(pedro, ap).
+sexta(henrique, ap).
+sexta(bia, ap).
+sexta(adriano, ap).
+sexta(alice, ap).
+sexta(bernado, ap).
+sexta(maria, ap).
+
+% dinheiro
+pobre(bernado).
+pobre(bia).
+pobre(pedro).
